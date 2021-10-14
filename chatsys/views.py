4cc43a051544d2c00d075b1efafc0260d9b9b3f0
Rahul Choudhary <rahul0101rock@gmail.com>
2021-10-14 18:35:09 +0000

    Changes in Django Settings, Added templates and created url.py for routing

--- chatsys/views.py
+++ chatsys/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def home(request):
+    return render(request,'chatsys/home.html',{})
