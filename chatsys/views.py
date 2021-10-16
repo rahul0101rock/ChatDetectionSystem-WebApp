76cf8454813b3f5b12d388a4c373ede1e5177499
Rahul Choudhary <rahul0101rock@gmail.com>
2021-10-16 20:28:49 +0000

    Added logIn Function

--- chatsys/views.py
+++ chatsys/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.contrib.auth import authenticate,login,logout
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
@@ -25,6 +26,22 @@ def signUp(request):
     		form = SignUpForm()
     	return render(request, 'chatsys/signup.html', {'form': form})
 
+def logIn(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('/')
+            else:
+                messages.error(request, 'Username or Password is Incorrect')
+                return redirect('login')
+        else:
+            return render(request,'chatsys/login.html',{})
 
 def logOut(request):
     if request.user.is_authenticated:
