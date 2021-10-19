509f400fd11efd40a2dcfeb32e2c1bd71702bc67
Rahul Choudhary <rahul0101rock@gmail.com>
2021-10-19 17:56:25 +0000

    Initialized firebase and added code to display and add default bio

--- chatsys/views.py
+++ chatsys/views.py
@@ -1,3 +1,4 @@
+import pyrebase
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth import authenticate,login,logout
@@ -5,6 +6,18 @@ from django.shortcuts import render, redirect
 from .forms import SignUpForm
 from django.contrib import messages
 # Create your views here.
+config={
+  "apiKey": "AIzaSyDCem5mrJqfv3phnowuLY1EK5vIzHdiY1o",
+  "authDomain": "chat-detection-system.firebaseapp.com",
+  "databaseURL": "https://chat-detection-system-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "projectId": "chat-detection-system",
+  "storageBucket": "chat-detection-system.appspot.com",
+  "messagingSenderId": "464189634762",
+  "appId": "1:464189634762:web:a726a15005bf5edd739c33",
+  "measurementId": "G-3H7P7CP7T1"
+}
+firebase=pyrebase.initialize_app(config)
+db=firebase.database()
 
 def home(request):
     return render(request,'chatsys/home.html',{})
@@ -21,6 +34,7 @@ def signUp(request):
     			password = form.cleaned_data['password1']
     			user = authenticate(username=username, password=password)
     			login(request,user)
+    			db.child("Bio").child(request.user).update({"bio":"Hello there!"})
     			return redirect('/')
     	else:
     		form = SignUpForm()
@@ -49,5 +63,7 @@ def logOut(request):
     return redirect('/')
 
 def profile(request):
-     return render(request,'chatsys/profile.html',{})
+    data={}
+    data["bio"]=db.child("Bio").child(request.user).get().val()['bio']
+    return render(request,'chatsys/profile.html',data)
 
